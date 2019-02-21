1eb59fd38b8c2610957a47dff06f37f4c157d248
LoreenaJ <loreena.jaouen@gmail.com>
2019-02-21 08:36:44 -0500

    Fixing a typo that prevented the app from getting the port number upon launch

--- utilities.js
+++ utilities.js
@@ -5,7 +5,7 @@ function getPort(){
 	var xhr = new XMLHttpRequest();
 	xhr.addEventListener("load",function(){
 		var parser = new DOMParser();
-		var doc = parser.parseFromString(xhr.responseText, "application.xml");
+		var doc = parser.parseFromString(xhr.responseText, "application/xml");
 		httpPortNumber = doc.getElementsByTagName("node-port-http").item(0).textContent;
 		httpsPortNumber = doc.getElementsByTagName("node-port-https").item(0).textContent;
 		alert("Port: " + httpPortNumber);
