453c17497945ab5f92e86a22ccd432bd18958927
LoreenaJ <loreena.jaouen@gmail.com>
2019-03-04 11:34:36 -0500

    Hardwiring the port number as it always shows as undefined

--- uploadData.js
+++ uploadData.js
@@ -44,7 +44,7 @@ function processData(postString){
 	//make a new request using the client variable
 	client = new XMLHttpRequest();
 	postString = postString+"&port_id="+httpPortNumber;
-	var url = 'http://developer.cege.ucl.ac.uk:'+httpPortNumber+'/uploadData';
+	var url = 'http://developer.cege.ucl.ac.uk:30282/uploadData/30282';
 	//use the server to bounce the data back to us using /reflectData
 	client.open('POST',url,true);
 	//inform the server of the type of data
