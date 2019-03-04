2dd56925b85bf48a45bc93a7a7b6b6f0d4f798d1
LoreenaJ <loreena.jaouen@gmail.com>
2019-03-04 04:36:25 -0500

    Testing by hardwiring the httpPortNumber to try and debug

--- leafletFunctions.js
+++ leafletFunctions.js
@@ -24,7 +24,7 @@ function addShapes(){
 
 function getFormData(){
 	client = new XMLHttpRequest();
-	client.open('GET','http://developer.cege.ucl.ac.uk:'+httpPortNumber+'/getFormData/'+httpPortNumber);
+	client.open('GET','http://developer.cege.ucl.ac.uk:30282/getFormData/30282');
 	client.onreadystatechange = earthquakeResponse;
 	client.send();
 }
