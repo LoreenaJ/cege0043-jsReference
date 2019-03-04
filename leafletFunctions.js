80dff652ba099572ca4c432882cce8c8a8816651
LoreenaJ <loreena.jaouen@gmail.com>
2019-03-04 04:44:09 -0500

    Trying with port number 30250 to see if points in Scotland show up

--- leafletFunctions.js
+++ leafletFunctions.js
@@ -25,7 +25,7 @@ function addShapes(){
 function getFormData(){
 	alert('Getting the data')
 	client = new XMLHttpRequest();
-	client.open('GET','http://developer.cege.ucl.ac.uk:'+httpPortNumber+'/getFormData/'+httpPortNumber);
+	client.open('GET','http://developer.cege.ucl.ac.uk:30250/getFormData/30250');
 	client.onreadystatechange = earthquakeResponse;
 	client.send();
 }
