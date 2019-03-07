3bc5d21eb3ae33a370f55218be3d5db532060f7a
LoreenaJ <loreena.jaouen@gmail.com>
2019-03-07 08:01:56 -0500

    Adding html pop ups

--- leafletFunctions.js
+++ leafletFunctions.js
@@ -26,7 +26,7 @@ function addShapes(){
 function getFormData(){
 	alert('Getting the data')
 	client = new XMLHttpRequest();
-	client.open('GET','http://developer.cege.ucl.ac.uk:'+httpPortNumber+'/postgistest');
+	client.open('GET','http://developer.cege.ucl.ac.uk:'+httpPortNumber+'/getGeoJSON/formdata/geom/'+httpPortNumber,true);
 	client.onreadystatechange = earthquakeResponse;
 	client.send();
 }
@@ -41,5 +41,26 @@ function earthquakeResponse(){
 function loadEarthquakelayer(earthquakedata){
 	var earthquakejson = JSON.parse(earthquakedata);
 	earthquakes = earthquakejson
-	earthquakelayer = L.geoJson(earthquakejson).addTo(mymap);
+	earthquakelayer = L.geoJson(earthquakejson,{
+		// use point to layer to create the points
+		pointToLayer: function (feature, latlng){
+		// in this case, we build an HTML DIV string
+		// using the values in the data
+			var htmlString = "<DIV id='popup'"+ feature.properties.id + "><h2>" + feature.properties.name + "</h2><br>";
+			htmlString = htmlString + "<h3>"+feature.properties.surname + "</h3><br>";
+			htmlString = htmlString + "<input type='radio' name='answer' id='"+feature.properties.id+"_1'/>"+feature.properties.module+"<br>";
+			htmlString = htmlString + "<input type='radio' name='answer' id='"+feature.properties.id+"_2'/>"+feature.properties.language+"<br>";
+			htmlString = htmlString + "<input type='radio' name='answer' id='"+feature.properties.id+"_3'/>"+feature.properties.lecturetime+"<br>";
+			htmlString = htmlString + "<input type='radio' name='answer' id='"+feature.properties.id+"_4'/>"+feature.properties.port_id+"<br>";
+			htmlString = htmlString + "<button onclick='checkAnswer(" + feature.properties.id + ");return false;'>Submit Answer</button>";
+			// now include a hidden element with the answer
+			// in this case the answer is alwasy the first choice
+			// for the assignment this will of course vary - you can use feature.properties.correct_answer
+			htmlString = htmlString + "<div id=answer" + feature.properties.id +
+			" hidden>1</div>";
+			htmlString = htmlString + "</div>";
+			return L.marker(latlng).bindPopup(htmlString);
+		},
+	}).addTo(mymap);
+	mymap.fitBounds(earthquakelayer.getBounds());
 }
