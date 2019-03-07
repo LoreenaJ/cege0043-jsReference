e52cdbbbca503583a1d39937f78c619a39bf0134
LoreenaJ <loreena.jaouen@gmail.com>
2019-03-07 09:25:47 -0500

    Adding a function to reflect the answer back to the user without saving it to the server

--- leafletFunctions.js
+++ leafletFunctions.js
@@ -64,3 +64,31 @@ function loadEarthquakelayer(earthquakedata){
 	}).addTo(mymap);
 	mymap.fitBounds(earthquakelayer.getBounds());
 }
+
+function checkAnswer(questionID) {
+	// get the answer from the hidden div
+	// NB - do this BEFORE you close the pop-up as when you close the pop-up the DIV is destroyed
+	var answer = document.getElementById("answer"+questionID).innerHTML;
+	// now check the question radio buttons
+	var correctAnswer = false;
+	var answerSelected = 0;
+	for (var i=1; i < 5; i++) {
+		if (document.getElementById(questionID+"_"+i).checked){
+			answerSelected = i;
+		}
+		if ((document.getElementById(questionID+"_"+i).checked) && (i == answer)) {
+			alert ("Well done");
+			correctAnswer = true;
+		}
+	}
+	if (correctAnswer === false) {
+	// they didn't get it right
+	alert("Better luck next time");
+	}
+	// now close the popup
+	mymap.closePopup();
+	// the code to upload the answer to the server would go here
+	// call an AJAX routine using the data
+	// the answerSelected variable holds the number of the answer
+	//that the user picked
+}
